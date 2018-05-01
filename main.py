e953402fde2bf919dff8a331d845b277b1b6685f
Zoltan <forsimi@gmail-com>
2018-05-01 16:15:16 +0200

    raspberry commit

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+#!/usr/bin/python3
+
 import scrap_stock
 import email_sender
+from time import sleep
 
 stock='vlkay'
-f=scrap_stock.scratch_page('https://www.marketwatch.com/investing/stock/'+stock,'intraday__close')
-dig = scrap_stock.filter_dig(f.find("td").text)
-str = scrap_stock.filter_str(f.find("td").text)
-t=str[0].encode('utf-8')
-print(t.decode('utf-8'))
-email_sender.send_email('cherrywidget@gmail.com','simi821219','forsimi@gmail.com','proba email kuldes','Stock: '+stock.upper()+' Price: '+dig[0]+' Currency: '+t.decode('utf-8'))
+s=7200
+
+try:
+    while True:
+        f=scrap_stock.scratch_page('https://www.marketwatch.com/investing/stock/'+stock,'intraday__close')
+        dig = scrap_stock.filter_dig(f.find("td").text)
+        str = scrap_stock.filter_str(f.find("td").text)
+
+        email_sender.send_email('cherrywidget@gmail.com','simi821219','forsimi@gmail.com','proba email kuldes','Stock: '+stock.upper()+' Price: '+dig[0]+' Currency: '+str[0]+' Your next mail in: 360 sec')
+        dig.clear()
+        str.clear()
+        sleep(s)
+except (KeyboardInterrupt):
+        print('Stopping...')
